a344956de68a053068413676b758a7b430231b34
FarkaschZoltan <farkasch.zoltan@gmail.com>
2026-01-26 21:32:56 +0100

    Fixed download script.

--- model-downloader/model-downloader.py
+++ model-downloader/model-downloader.py
@@ -1,31 +1,28 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
 
 model_id = "meta-llama/Llama-3.2-3B"
 
 tokenizer_clean = AutoTokenizer.from_pretrained(
-    model_id,
-    use_auth_token=True
+    model_id
 )
 
 model_clean = AutoModelForCausalLM.from_pretrained(
     model_id,
-    use_auth_token=True,
-    cache_dir="C:/Users/farka/OneDrive/Egyetem/MSC/diplomamunka/Models/llama3.2-3b-clean"
+    cache_dir="C:/Users/farka/Projects/Diplomamunka/Models/llama3.2-3b-clean"
 )
 
-tokenizer_clean.save_pretrained("C:/Users/farka/OneDrive/Egyetem/MSC/diplomamunka/Models/llama3.2-3b-clean")
-model_clean.save_pretrained("C:/Users/farka/OneDrive/Egyetem/MSC/diplomamunka/Models/llama3.2-3b-clean")
+tokenizer_clean.save_pretrained("C:/Users/farka/Projects/Diplomamunka/Models/llama3.2-3b-clean")
+model_clean.save_pretrained("C:/Users/farka/Projects/Diplomamunka/Models/llama3.2-3b-clean")
 
 tokenizer_lora = AutoTokenizer.from_pretrained(
-    model_id,
-    use_auth_token=True
+    model_id
 )
 
 model_lora = AutoModelForCausalLM.from_pretrained(
     model_id,
-    use_auth_token=True,
-    cache_dir="C:/Users/farka/OneDrive/Egyetem/MSC/diplomamunka/Models/llama3.2-3b-lora"
+    cache_dir="C:/Users/farka/Projects/Diplomamunka/Models/llama3.2-3b-lora"
 )
 
-tokenizer_lora.save_pretrained("C:/Users/farka/OneDrive/Egyetem/MSC/diplomamunka/Models/llama3.2-3b-lora")
-model_lora.save_pretrained("C:/Users/farka/OneDrive/Egyetem/MSC/diplomamunka/Models/llama3.2-3b-lora")
+tokenizer_lora.save_pretrained("C:/Users/farka/Projects/Diplomamunka/Models/llama3.2-3b-lora")
+model_lora.save_pretrained("C:/Users/farka/Projects/Diplomamunka/Models/llama3.2-3b-lora")
